d6f2659db9ad085d16627ff9262c169d5aa23104
Dylan Bennett <dylan.bennett@stfc.ac.uk>
2022-11-24 12:09:18 +0000

    No Guest Access Update

--- ckanext/smdh/plugin.py
+++ ckanext/smdh/plugin.py
@@ -1,9 +1,11 @@
 import ckan.plugins as plugins
 import ckan.plugins.toolkit as toolkit
 
+import ckanext.smdh.middleware as smdh_middleware
 
 class SmdhPlugin(plugins.SingletonPlugin):
     plugins.implements(plugins.IConfigurer)
+    plugins.implements(plugins.IMiddleware)
 
     # IConfigurer
 
@@ -12,3 +14,11 @@ class SmdhPlugin(plugins.SingletonPlugin):
         toolkit.add_public_directory(config_, 'public')
         toolkit.add_resource('fanstatic',
             'smdh')
+
+    # IMiddleware
+
+    def make_middleware(self, app, config):
+        return smdh_middleware.AuthMiddleware(app, config)
+
+    def make_error_log_middleware(self, app, config):
+        return smdh_middleware.AuthMiddleware(app, config)
