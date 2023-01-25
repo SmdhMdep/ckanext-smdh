c25a0df000b708ec5903edd623ae7f0868809d3f
Dylan Bennett <dylan.bennett@stfc.ac.uk>
2023-01-25 09:57:23 +0000

    Modifies the way webassets are servdr and adds partialmdep branding

--- ckanext/smdh/plugin.py
+++ ckanext/smdh/plugin.py
@@ -10,10 +10,22 @@ class SmdhPlugin(plugins.SingletonPlugin):
     # IConfigurer
 
     def update_config(self, config_):
+        # Add this plugin's templates dir to CKAN's extra_template_paths, so
+        # that CKAN will use this plugin's custom templates.
         toolkit.add_template_directory(config_, 'templates')
+
+        # Add this plugin's public dir to CKAN's extra_public_paths, so
+        # that CKAN will use this plugin's custom static files.
         toolkit.add_public_directory(config_, 'public')
-        toolkit.add_resource('fanstatic',
-            'smdh')
+
+        # toolkit.add_resource('fanstatic', 'smdh')
+
+        # Register this plugin's assets directory with CKAN.
+        # Here, 'assets' is the path to the webassets directory
+        # (relative to this plugin.py file), and 'smdh' is the name
+        # that we'll use to refer to this assets directory from CKAN
+        # templates.
+        toolkit.add_resource('assets', 'smdh')
 
     # IMiddleware
 
