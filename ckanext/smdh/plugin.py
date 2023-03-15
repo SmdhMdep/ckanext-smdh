d1938dd693205a10bf8c3f66ad46b98f5a02595a
Dylan Bennett <dylan.bennett@stfc.ac.uk>
2023-03-15 13:21:03 +0000

    Old code clean up

--- ckanext/smdh/plugin.py
+++ ckanext/smdh/plugin.py
@@ -1,13 +1,11 @@
 import ckan.plugins as plugins
 import ckan.plugins.toolkit as toolkit
 
-import ckanext.smdh.middleware as smdh_middleware
 import ckanext.smdh.helpers as helpers
 
 class SmdhPlugin(plugins.SingletonPlugin):
     plugins.implements(plugins.IConfigurer)
     plugins.implements(plugins.ITemplateHelpers, inherit=True)
-    #plugins.implements(plugins.IMiddleware)
 
     # IConfigurer
 
@@ -28,14 +26,6 @@ class SmdhPlugin(plugins.SingletonPlugin):
         # that we'll use to refer to this assets directory from CKAN
         # templates.
         toolkit.add_resource('assets', 'smdh')
-
-    # IMiddleware
-
-    #def make_middleware(self, app, config):
-    #    return smdh_middleware.AuthMiddleware(app, config)
-
-    #def make_error_log_middleware(self, app, config):
-    #    return smdh_middleware.AuthMiddleware(app, config)
 
     # ITemplateHelpers
     def get_helpers(self):
